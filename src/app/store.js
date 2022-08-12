b4ebbbef69d6e2baa2ea260b497eae77eb8f8cd5
VincentVanN <vinc.vannieu@gmail.com>
2022-08-12 16:02:12 +0200

    user slice created

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from '../feature/products.slice';
+import userReducer from '../feature/user.slice';
 
 export default configureStore({
   reducer: {
     products: productsReducer,
+    user: userReducer,
   },
 });
