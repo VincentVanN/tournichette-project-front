5ff090e845d7dfc0b9c5c6a1fea8d865f3090913
J-Pajot <pajot.julie@gmail.com>
2022-08-16 14:12:09 +0200

    findProduct

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,24 +1,38 @@
 // import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
+import { useSelector } from 'react-redux';
 import './product.scss';
+import Page from 'src/components/Page/Page';
 
-function Product(name, image, price, unity, stock) {
-  // const oneProduct = useSelector((state) => state.products.products).filter((element) => element === slug );
+function Product() {
+  const { slug } = useParams();
+  const products = useSelector((state) => state.products.products);
+  const oneProduct = products.find((element) => element.slug === slug);
 
   const navigate = useNavigate();
   const handleClickCart = () => navigate('/panier');
   return (
-    <div className="product">
-      <article className="card_article">
-        product
-      </article>
-      <button
-        type="button"
-        onClick={handleClickCart}
-      >
-        Mettre dans mon panier
-      </button>
-    </div>
+    <Page>
+      <div className="product">
+        <article className="product_article">
+          <h2 className="product-title">{oneProduct.name}</h2>
+          <img className="product-img" src={oneProduct.image} alt={oneProduct.name} />
+          <ul className="product-infos">
+            <li>{oneProduct.price}</li>
+            <li>{oneProduct.unity}</li>
+            <li>{oneProduct.stock}</li>
+          </ul>
+
+        </article>
+        <button
+          type="button"
+          onClick={handleClickCart}
+          className="product_button"
+        >
+          Mettre dans mon panier
+        </button>
+      </div>
+    </Page>
   );
 }
 
