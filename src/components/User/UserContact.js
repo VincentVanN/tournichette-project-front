b5339f71aa41b3495c978bb4c182f44fe2d85767
VincentVanN <vinc.vannieu@gmail.com>
2022-08-17 11:48:36 +0200

    dynamic filed in userContact done

--- src/components/User/UserContact.js
+++ src/components/User/UserContact.js
@@ -1,33 +1,51 @@
 import { useSelector } from 'react-redux';
 import './user.scss';
+import { useState } from 'react';
 import Page from '../Page/Page';
 
 function UserContact() {
-  const user = useSelector((state) => state.user.user);
+  const {
+    firstname, lastname, phone, email,
+  } = useSelector((state) => state.user.user);
+  const [isForm, setIsForm] = useState(false);
+  const handleClick = () => {
+    setIsForm(!isForm);
+  };
+  const hiddenForm = !isForm ? 'hidden' : '';
+  const hiddenLi = isForm ? 'hidden' : '';
   return (
     <Page>
       <form>
         <ul className="updateUserAccount">
           <div className="updateUserAccount container">
-            <li className="updateUserAccount list-item">{user.firstname}</li>
-            <input />
+            <li className={`updateUserAccount list-item ${hiddenLi}`}>{firstname}</li>
+            <input
+              className={`updateUserAccount input-item ${hiddenForm}`}
+            />
           </div>
           <div className="updateUserAccount container">
-            <li className="updateUserAccount list-item">{user.lastname}</li>
-            <input />
+            <li className={`updateUserAccount list-item ${hiddenLi}`}>{lastname}</li>
+            <input
+              className={`updateUserAccount input-item ${hiddenForm}`}
+            />
           </div>
           <div className="updateUserAccount container">
-            <li className="updateUserAccount list-item">{user.phone}</li>
-            <input />
+            <li className={`updateUserAccount list-item ${hiddenLi}`}>{phone}</li>
+            <input
+              className={`updateUserAccount input-item ${hiddenForm}`}
+            />
           </div>
           <div className="updateUserAccount container">
-            <li className=" list-item">{user.email}</li>
-            <input />
+            <li className={`updateUserAccount list-item ${hiddenLi}`}>{email}</li>
+            <input
+              className={`updateUserAccount input-item ${hiddenForm}`}
+            />
           </div>
         </ul>
         <button
           className="updateUserAccount button"
           type="button"
+          onClick={handleClick}
         > Mettre à jour mes informations
         </button>
       </form>
