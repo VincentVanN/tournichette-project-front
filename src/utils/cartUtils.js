ea5eefb3ba83a340dad99452a80797cef89b3a0e
VincentVanN <vinc.vannieu@gmail.com>
2022-08-22 13:53:20 +0200

    algo for products navigation done

--- src/utils/cartUtils.js
+++ src/utils/cartUtils.js
@@ -6,12 +6,18 @@ export const changeQuantityProduct = (list, product, quantity) => {
     const newItem = { ...item };
     newItem.quantity += quantity;
     const newArrayForState = productsCopy.filter((element) => element.id !== product.id);
-    if (newItem.quantity > 0) {
-      newArrayForState.push(newItem);
+    if (newItem.quantity <= 0) {
       return newArrayForState;
     }
+    newArrayForState.push(newItem);
     return newArrayForState;
   }
   productsCopy.push(product);
   return productsCopy;
+};
+
+export const navigationInProduct = (products, product, increment) => {
+  const indexOfActuallyProduct = products.indexOf(product);
+  const nextProduct = products[(indexOfActuallyProduct + increment)];
+  return nextProduct.slug;
 };
