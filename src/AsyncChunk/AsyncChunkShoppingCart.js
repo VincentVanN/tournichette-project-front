6784ea9d0144942ce1b726455ded1ab0744c22b7
VincentVanN <vinc.vannieu@gmail.com>
2022-08-24 12:06:20 +0200

    post order request

--- src/AsyncChunk/AsyncChunkShoppingCart.js
+++ src/AsyncChunk/AsyncChunkShoppingCart.js
@@ -15,3 +15,29 @@ export const getDepotsList = createAsyncThunk(
     return result.data;
   },
 );
+export const postOrder = createAsyncThunk(
+  'shoppingCart/postOrder',
+  async (_, { getState }) => {
+    const { token } = getState().user.user;
+    const cartOrders = [];
+    //
+    const orderProducts = getState().shoppingCart.shoppingCart
+      .map((product) => ({ quantity: product.quantity.toString(), id: product.id }));
+    //
+    let order = {
+      price: getState().shoppingCart.cartAmount.toString(),
+      depot: getState().shoppingCart.selectedDepotId,
+      orderProducts,
+      cartOrders,
+    };
+    order = JSON.stringify(order);
+    const result = await axios.post('http://localhost:8000/api/v1/orders/create', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        order,
+      },
+    });
+    console.log(result.data);
+    return result.data;
+  },
+);
