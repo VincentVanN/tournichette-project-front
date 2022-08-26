6bbb3488dd393e68e411f8683cee1590ec73e477
VincentVanN <vinc.vannieu@gmail.com>
2022-08-26 11:17:02 +0200

    add nalink in sidebar and fix css

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,26 +2,7 @@ import './loading.scss';
 
 function Loading() {
   return (
-    <div className="loading">
-      <div className="ninjin">
-        <div className="head">
-          <span />
-          <span />
-          <span />
-        </div>
-        <div className="ninjin__body">
-          <div />
-          <span />
-        </div>
-        <div className="ninjin__leg ninjin__leg--l">
-          <div />
-        </div>
-        <div className="ninjin__leg ninjin__leg--r">
-          <div />
-        </div>
-      </div>
-    </div>
-
+    <div className="chaotic-orbit" />
   );
 }
 
