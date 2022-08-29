2efcc8ed316b6815e34bfe0a00c7234c75dd610d
J-Pajot <pajot.julie@gmail.com>
2022-08-29 09:32:07 +0200

    css not finish

--- src/components/DevTeam/DevTeam.js
+++ src/components/DevTeam/DevTeam.js
@@ -1,10 +1,5 @@
 import Page from '../Page/Page';
 import 'src/components/DevTeam/devTeam.scss';
-import vincent from './vincent.jpeg';
-import julie from './julie.jpeg';
-import sophie from './sophie.jpg';
-import steve from './steve.jpeg';
-import florent from './florent.jpg';
 
 function DevTeam() {
   return (
@@ -15,28 +10,27 @@ function DevTeam() {
         <div className="devTeam_cards">
           <div className="devTeam_card">
             <h3>Vincent Van Nieuwenborgh</h3>
-            <img className="devTeam_image" src={vincent} alt="vincent" />
-            <a href="https://www.linkedin.com/in/vincent-van-nieuwenborgh-1b45a4243/">Linkedin</a>
+            <a href="https://www.linkedin.com/in/vincent-van-nieuwenborgh-1b45a4243/">
+              <ion-icon name="logo-linkedin" />
+            </a>
           </div>
           <div className="devTeam_card">
             <h3>Florent Genestier</h3>
-            <img className="devTeam_image" src={florent} alt="florent" />
-            <a href="https://www.linkedin.com/in/florent-genestier/">Linkedin</a>
+            <a href="https://www.linkedin.com/in/florent-genestier/">
+              <ion-icon name="logo-linkedin" />
+            </a>
           </div>
           <div className="devTeam_card">
             <h3>Steve Amat</h3>
-            <img className="devTeam_image" src={steve} alt="steve" />
-            <a href="https://www.linkedin.com/in/steve-amat/">Linkedin</a>
-          </div>
-          <div className="devTeam_card">
-            <h3>Sophie Charles</h3>
-            <img className="devTeam_image" src={sophie} alt="sophie" />
-            <a href="https://www.linkedin.com/in/sophiecharles-webdeveloper/">Linkedin</a>
+            <a href="https://www.linkedin.com/in/steve-amat/">
+              <ion-icon name="logo-linkedin" />
+            </a>
           </div>
           <div className="devTeam_card">
             <h3>Julie Pajot</h3>
-            <img className="devTeam_image" src={julie} alt="julie" />
-            <a href="https://www.linkedin.com/in/julie-pajot/">Linkedin</a>
+            <a href="https://www.linkedin.com/in/julie-pajot/">
+              <ion-icon name="logo-linkedin" />
+            </a>
           </div>
         </div>
         <p className="devTeam_info">Si vous souhaitez nous contacter pour créer des sites merveilleux, appelez nous!!  </p>
