6e7482e6ab9c1d8cc3b12c671095c52ddf010336
J-Pajot <pajot.julie@gmail.com>
2022-08-29 16:48:08 +0200

    map orderHistory

--- src/components/User/Orders.js
+++ src/components/User/Orders.js
@@ -4,31 +4,51 @@ import './user.scss';
 function Orders() {
   const orderHistory = useSelector((state) => state.user.orderHistory);
   console.log(orderHistory);
-  return (
-    <div className="orders">
-      <h2>historique des commandes</h2>
-      <div className="orderHistory">
-        {orderHistory.data.map((order) => (
-          <>
-            <div
-              className="date"
-              key={order.id}
-            >{order.dateOrder}
-            </div>
-            {/* <div className="cartOrders">{order.cartOrders.cart.name}</div> */}
-            <div
-              className="price"
 
-            >
-              {order.price}€
+  // orderHistory.forEach((element) => newArray.push(element.cartOrders));
+  // console.log(newArray);
+  // const newArray = orderHistory.orderProducts;
+
+  if (orderHistory.length === 0) {
+    return (
+      <>
+        <h2>historique des commandes</h2>
+        <p>Vous n'avez pas encore commandé :D </p>
+      </>
+    );
+  }
+  if (orderHistory.length !== 0) {
+    return (
+      <div className="orders">
+        <h2>historique des commandes</h2>
+        <div className="orderHistory">
+          {orderHistory.data.map((order) => (
+            <div className="orderHistory-one" key={order.id}>
+              <ul>
+                <li className="orderHistory-date">Commande du {order.dateOrder}</li>
+                <li className="orderHistory-depot">Déposé au {order.depot.address}</li>
+                { order.orderProducts.map((product) => (
+                  <li
+                    key={product.product.slug}
+                  >
+                    {product.quantity} {product.product.name} : {product.product.price}€ par lot
+                  </li>
+                ))}
+                { order.cartOrders.map((cart) => (
+                  <li
+                    key={cart.cart.name}
+                  >
+                    {cart.quantity} {cart.cart.name} : {cart.cart.price}€ par lot
+                  </li>
+                ))}
+                <li className="orderHistory-price">Prix total: {order.price}€</li>
+              </ul>
             </div>
-            {/* <div className="depot">{order.depot.adress}</div> */}
-          </>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
 
-  );
+    );
+  }
 }
-
 export default Orders;
