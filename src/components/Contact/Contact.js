439df03312305fcf2e52e9a8c1f94504cca4da4b
J-Pajot <pajot.julie@gmail.com>
2022-08-31 14:31:31 +0200

    margin contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,7 +9,7 @@ function Contact() {
         <h2>CONTACTEZ-NOUS !</h2>
         <p className="contact-paragraph">
           Vous avez faim de bio légumes ? Vous voulez plus de renseignement sur nos paniers ? nous faire part d’une idée,  d’une critique ou de votre admiration sans bornes ? nous soutenir ou nous encourager ? Ou alors vous aimeriez partager vos propres aventures ? 🙂 Écrivez-nous, nous nous ferons un plaisir de vous répondre !
-          Pour une réponse rapide (commande, etc.), privilégiez toutefois le téléphone:
+          Pour une réponse rapide (commande, etc.), privilégiez toutefois le téléphone au
         </p>
         <p className="contact_info">0981731287</p>
         <p>
