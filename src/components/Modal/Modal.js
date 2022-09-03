f68cbc9cdafd831d00f07b44d9f47e3b3ade64b3
VincentVanN <vinc.vannieu@gmail.com>
2022-09-03 19:51:43 +0200

    modal wip

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,12 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import PropTypes from 'prop-types';
 
-function Modal({ message, handler }) {
+function Modal({
+  message,
+  handler,
+  textButton,
+  icon,
+}) {
   const backdrop = {
     visible: { opacity: 1 },
     hidden: { opacity: 0 },
@@ -13,12 +18,25 @@ function Modal({ message, handler }) {
         variants={backdrop}
         initial="hidden"
         animate="visible"
-      />
+      >
+        <motion.div className="modal">
+          <p>{message}</p>
+          <button
+            type="button"
+            onClick={handler}
+          >
+            <p>{textButton}</p>
+            <ion-icon name={icon} />
+          </button>
+        </motion.div>
+      </motion.div>
     </AnimatePresence>
   );
 }
 Modal.propTypes = {
   message: PropTypes.string.isRequired,
+  textButton: PropTypes.string.isRequired,
   handler: PropTypes.func.isRequired,
+  icon: PropTypes.string.isRequired,
 };
 export default Modal;
