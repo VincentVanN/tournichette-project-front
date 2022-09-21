b6d503e04baa61d9c0269fae0c0d16e8f8befba7
VincentVanN <vinc.vannieu@gmail.com>
2022-09-21 15:13:14 +0200

    fix credit card array tro display

--- src/components/ShoppingCart/ShoppingCartPaymentSuccess.js
+++ src/components/ShoppingCart/ShoppingCartPaymentSuccess.js
@@ -1,6 +1,5 @@
 import { useLocation, useNavigate } from 'react-router';
 import { motion } from 'framer-motion';
-import './shoppingCart.scss';
 
 function ShoppingCartPaymentSuccess() {
   const navigate = useNavigate();
@@ -9,36 +8,36 @@ function ShoppingCartPaymentSuccess() {
   const { state } = useLocation();
   return (
     <div className="shoppingCart">
-      <div
-        className="shoppingCart-empty-title"
-      >
-        {!state || state.origin === 'creditCard' ? 'Paiement effectué! Merci pour tes achats' : 'Commande enregistrée! Merci pour ta fidélité'}
-      </div>
       <motion.div
         className="shoppingCart-empty"
-        onClick={handleClickHome}
         initial={{ x: '-100vw' }}
         animate={{ x: 0 }}
       >
         <div
+          className="shoppingCart-empty-title"
+        >
+          {!state || state.origin === 'creditCard' ? 'Paiement effectué! Merci pour tes achats' : 'Commande enregistrée! Merci pour ta fidélité'}
+        </div>
+        <div
           className="shoppingCart-empty-content"
+          onClick={handleClickHome}
         >
           Retour à l'accueil
         </div>
         <ion-icon name="arrow-forward-circle-outline" size="large" />
-      </motion.div>
-      <motion.div
-        className="shoppingCart-empty"
-        onClick={handleClickProfil}
-        initial={{ x: '100vw' }}
-        animate={{ x: 0 }}
-      >
         <div
-          className="shoppingCart-empty-content"
+          className="shoppingCart-empty"
+          onClick={handleClickProfil}
+          initial={{ x: '100vw' }}
+          animate={{ x: 0 }}
         >
-          Détail des commandes
+          <div
+            className="shoppingCart-empty-content"
+          >
+            Détail des commandes
+          </div>
+          <ion-icon name="arrow-forward-circle-outline" size="large" />
         </div>
-        <ion-icon name="arrow-forward-circle-outline" size="large" />
       </motion.div>
     </div>
   );
