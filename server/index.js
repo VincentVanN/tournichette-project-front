0852583e1fb78d5931ab8ed4258f56ec3e596e59
VincentVanN <vinc.vannieu@gmail.com>
2022-10-18 15:57:44 +0200

    test cors

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
 // Dependencies
 const fs = require('fs');
 const http = require('http');
-const https = require('https');
+// const https = require('https');
 const express = require('express');
 
 const app = express();
 
-// Certificate
-const privateKey = fs.readFileSync('/home/pvfq7304/tournichette.fr/projet-tournichette-front/server/key.pem', 'utf8');
-const certificate = fs.readFileSync('/home/pvfq7304/tournichette.fr/projet-tournichette-front/server/cert.pem', 'utf8');
-const ca = fs.readFileSync('/home/pvfq7304/tournichette.fr/projet-tournichette-front/server/chain.pem', 'utf8');
+// // Certificate
+// const privateKey = fs.readFileSync('/home/pvfq7304/tournichette.fr/projet-tournichette-front/server/key.pem', 'utf8');
+// const certificate = fs.readFileSync('/home/pvfq7304/tournichette.fr/projet-tournichette-front/server/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/home/pvfq7304/tournichette.fr/projet-tournichette-front/server/chain.pem', 'utf8');
 
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
+// const credentials = {
+//   key: privateKey,
+//   cert: certificate,
+//   ca: ca,
+// };
 
 app.use((req, res) => {
   res.send('Hello there !');
@@ -23,15 +23,15 @@ app.use((req, res) => {
 
 // Starting both http & https servers
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
   console.log('HTTP Server running on port 80');
 });
 
-httpsServer.listen(443, () => {
-  console.log('HTTPS Server running on port 443');
-});
+// httpsServer.listen(443, () => {
+//   console.log('HTTPS Server running on port 443');
+// });
 
 /* eslint-disable max-len */
 // if (typeof (PhusionPassenger) !== 'undefined') {
